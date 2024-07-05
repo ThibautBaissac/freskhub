a990602228377cfade32fe3baf3e30f595f91d3d
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-07-05 16:41:46 +0200

    Create Constants::Regex::EMAIL

---
+++ app/classes/constants/regex.rb
@@ -0,0 +1,6 @@
+module Constants
+  module Regex
+    # based on URI::MailTo::EMAIL_REGEXP from Ruby standard lib but with mandatory TLD
+    EMAIL = /\A[a-zA-Z0-9.!\#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+\z/
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,13 @@ class User < ApplicationRecord
   encrypts :email, deterministic: true, downcase: true
 
   validates :uuid, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
   validates :ui_language, presence: true, inclusion: {in: %w[fr en]}
   validates :country, presence: true
   validates :latitude, numericality: {greater_than_or_equal_to: -90, less_than_or_equal_to: 90}, allow_nil: true
   validates :longitude, numericality: {greater_than_or_equal_to: -180, less_than_or_equal_to: 180}, allow_nil: true
-  validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}
+  validates :email, presence: true, uniqueness: true,
+                    format: {with: Constants::Regex::EMAIL, message: "must be a valid email address"}
+
   validate :last_login_at_cannot_be_in_the_future
 
   private
