aa972b0e27145503631b57f9f3c152d59beeee68
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-07-05 15:14:44 +0200

    Create country_spec.rb

---
+++ spec/factories/countries.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :country do
+    name { "France" }
+    alpha2_code { "FR" }
+    alpha3_code { "FRA" }
+  end
+end
---
+++ spec/models/country_spec.rb
@@ -0,0 +1,28 @@
+# spec/models/country_spec.rb
+require "rails_helper"
+
+RSpec.describe Country, type: :model do
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:alpha2_code) }
+    it { should validate_presence_of(:alpha3_code) }
+
+    it { should validate_uniqueness_of(:name) }
+    it { should validate_uniqueness_of(:alpha2_code) }
+    it { should validate_uniqueness_of(:alpha3_code) }
+  end
+
+  describe "associations" do
+    it { should have_many(:users).dependent(:restrict_with_exception) }
+  end
+
+  describe "default scope" do
+    pending "orders by name ascending"
+    #   let!(:country2) { create(:country, name: "Country 1", alpha2_code: "C1", alpha3_code: "CU1") }
+    #   let!(:country1) { create(:country, name: "Country 2", alpha2_code: "C2", alpha3_code: "CU2") }
+
+    #   it "orders by name ascending" do
+    #     expect(Country.all).to eq([country2, country1])
+    #   end
+  end
+end
