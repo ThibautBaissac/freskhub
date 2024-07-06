8da350d74139583f257eb51594a9ba42dd8293a4
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-07-06 10:41:56 +0200

    Add relations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,14 @@ class User < ApplicationRecord
   has_many :user_languages
   has_many :languages, through: :user_languages
 
+  has_many :roles, class_name: "TrainingSession::Role", foreign_key: "user_id"
+  has_many :editor_roles, class_name: "TrainingSession::Editor", foreign_key: "user_id"
+  has_many :facilitator_roles, class_name: "TrainingSession::Facilitator", foreign_key: "user_id"
+  has_many :participant_roles, class_name: "TrainingSession::Participant", foreign_key: "user_id"
+  has_many :edited_sessions, through: :editor_roles, source: :training_session
+  has_many :facilitated_sessions, through: :facilitator_roles, source: :training_session
+  has_many :participated_sessions, through: :participant_roles, source: :training_session
+
   encrypts :email, deterministic: true, downcase: true
 
   validates :uuid, presence: true, uniqueness: true
