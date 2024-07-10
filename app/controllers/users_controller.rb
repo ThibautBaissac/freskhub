dc0a6f8b4d58df3bcbf02ccfc803811cf3c8ff8e
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-07-10 15:03:44 +0200

    Implement users#show edit and update

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update]
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      render :show
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :firstname, :lastname, :description, :locale, :latitude, :longitude, :address,
+                                 :city, :state, :postal_code, :country_id)
+  end
+end
