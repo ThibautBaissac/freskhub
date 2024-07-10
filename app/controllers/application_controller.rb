8c6f0f1bafce9e0d530ba5892674d6166731512a
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-07-10 10:30:58 +0200

    Create auth methods in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,36 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
+  before_action :authenticate_user!
 
   private
 
+  def authenticate_user!
+    redirect_to new_auth_session_path, alert: "You must sign in or sign up first!" unless user_signed_in?
+  end
+
   def current_user
+    Current.user ||= authenticate_user_from_session
+  end
+  helper_method :current_user
+
+  def authenticate_user_from_session
+    User.find_by(id: session[:user_id])
+  end
+
+  def user_signed_in?
+    current_user.present?
+  end
+  helper_method :user_signed_in?
+
+  def login(user)
+    Current.user = user
+    reset_session
+    session[:user_id] = user.id
+  end
+
+  def logout(_user)
+    Current.user = nil
+    reset_session
   end
 
   def current_user_locale
@@ -28,6 +55,6 @@ class ApplicationController < ActionController::Base
   end
 
   def default_url_options
-    { locale: I18n.locale }
+    {locale: I18n.locale}
   end
 end
