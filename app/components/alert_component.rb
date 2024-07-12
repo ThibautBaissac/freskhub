3f92fd1abd06886a424452fb2e73e3af78e5bc83
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-07-12 15:12:37 +0200

    Create alert_component

---
+++ app/components/alert_component.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+class AlertComponent < ViewComponent::Base
+  def initialize(message:, type: :primary, size: :md, closable: true)
+    @message = message
+    @type = type
+    @size = size
+    @closable = closable
+  end
+
+  def bg_color
+    {
+      primary: "bg-indigo-200",
+      secondary: "bg-gray-300",
+      success: "bg-green-200",
+      warning: "bg-amber-200",
+      danger: "bg-red-800"
+    }[@type]
+  end
+
+  def text_color
+    {
+      primary: "text-indigo-800",
+      secondary: "text-gray-900",
+      success: "text-green-800",
+      warning: "text-amber-800",
+      danger: "text-red-100"
+    }[@type]
+  end
+
+  def border_color
+    {
+      primary: "border-indigo-400",
+      secondary: "border-gray-400",
+      success: "border-green-400",
+      warning: "border-amber-400",
+      danger: "border-red-900"
+    }[@type]
+  end
+end
