7ce4bb03588bd559c9f7d22c3d991095293adfc4
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-07-12 14:13:39 +0200

    Implement locale_controller and logic

---
+++ app/controllers/users/locale_controller.rb
@@ -0,0 +1,15 @@
+class Users::LocaleController < ApplicationController
+  def edit
+  end
+
+  def update
+    if current_user && params[:new_locale].in?(I18n.available_locales.map(&:to_s))
+      current_user.update(locale: params[:new_locale])
+      I18n.locale = params[:new_locale]
+      session[:locale] = I18n.locale
+      redirect_back_or_to root_path
+    else
+      redirect_to root_path, alert: "Invalid locale or user."
+    end
+  end
+end
