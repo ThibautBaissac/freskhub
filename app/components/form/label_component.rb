62d78afad7d4682f6e8309a0aa2ab2238592cd3d
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-07-16 15:42:32 +0200

    Create Form::LabelComponent

---
+++ app/components/form/label_component.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class Form::LabelComponent < ViewComponent::Base
+  def initialize(form:, label: nil, value: "", classes: "block text-sm font-medium text-gray-700")
+    @form = form
+    @label = label
+    @value = value
+    @classes = classes
+  end
+end
