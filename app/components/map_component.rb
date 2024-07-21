08876de6140e4439f0f1767be089098385c95c28
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-07-21 14:38:30 +0200

    Update height in map_component

--- app/components/map_component.rb
+++ app/components/map_component.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class MapComponent < ViewComponent::Base
-  def initialize(geocoded_instances:, popup_content: nil)
+  def initialize(geocoded_instances:, popup_content: nil, height: "400px")
     @geocoded_instances = geocoded_instances
     @popup_content = popup_content
+    @height = height
   end
 end
