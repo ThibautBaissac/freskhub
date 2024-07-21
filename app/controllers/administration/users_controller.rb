3ea22475e98a26b8667449aa79bfb17c839e2f71
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-07-21 14:18:59 +0200

    Fix user scope typo "geocoded"

--- app/controllers/administration/users_controller.rb
+++ app/controllers/administration/users_controller.rb
@@ -1,6 +1,6 @@
 class Administration::UsersController < ApplicationController
   def index
     authorize(%i[administration user], :index?)
-    @geocoded_users = User.in_same_fresk_as(current_user).gecoded
+    @geocoded_users = User.in_same_fresk_as(current_user).geocoded
   end
 end
--- app/models/concerns/user/scopes.rb
+++ app/models/concerns/user/scopes.rb
@@ -2,7 +2,7 @@ module User::Scopes
   extend ActiveSupport::Concern
 
   included do
-    scope :gecoded, -> { where.not(longitude: nil, latitude: nil) }
+    scope :geocoded, -> { where.not(longitude: nil, latitude: nil) }
     scope :in_same_fresk_as, lambda { |user|
                                joins(:user_infos)
                                  .where(user_infos: {fresk_id: user.user_infos.select(:fresk_id)})
