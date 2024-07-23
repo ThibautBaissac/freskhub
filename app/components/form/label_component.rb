5bd3a8a762f05e0624f6c70fa5a2e529e753be59
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-07-23 14:38:26 +0200

    Remove for_input from label component

--- app/components/form/label_component.rb
+++ app/components/form/label_component.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 class Form::LabelComponent < ViewComponent::Base
-  def initialize(form:, label: nil, value: "", classes: "block text-sm font-medium text-gray-700 mb-1", for_input: nil)
+  def initialize(form:, label: nil, value: "", classes: "block text-sm font-medium text-gray-700 mb-1")
     @form = form
     @label = label
     @value = value
     @classes = classes
-    @for_input = for_input
   end
 end
