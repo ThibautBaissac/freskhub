adba4a7b3c7ef0dc73fad9a5739477f07228195d
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-07-24 11:12:26 +0200

    Remove fill attribute from all SVG components

--- app/components/svg/base_component.rb
+++ app/components/svg/base_component.rb
@@ -1,7 +1,6 @@
 class Svg::BaseComponent < ViewComponent::Base
-  def initialize(classes: "w-8 h-8 stroke-gray-600", stroke_width: 2, fill: "none")
+  def initialize(classes: "w-8 h-8 stroke-gray-600", stroke_width: 2)
     @classes = classes
-    @fill = fill
     @stroke_width = stroke_width
   end
 end
