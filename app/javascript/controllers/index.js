25b9a02dd9cec2638110e80a0ef15f4203539fd1
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-07-26 10:55:20 +0200

    Implement AccordionComponent in _search_and_filter_accordion.html

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import AccordionController from "./accordion_controller"
+application.register("accordion", AccordionController)
+
 import DialogController from "./dialog_controller"
 application.register("dialog", DialogController)
 
