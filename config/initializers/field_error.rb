4ab188edbece603039c9846fa90e4c34f8b5277c
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-07-29 09:38:43 +0200

    Create field_error.rb initializer

---
+++ config/initializers/field_error.rb
@@ -0,0 +1,18 @@
+ActionView::Base.field_error_proc = proc do |html_tag, instance_tag|
+  fragment = Nokogiri::HTML.fragment(html_tag)
+  field = fragment.at("input,select,textarea")
+
+  html = if field
+           error_message = instance_tag.error_message.join(", ")
+           field["class"] = "#{field['class']} border-red-600 border focus:outline-none"
+           html = <<-HTML
+              #{fragment}
+              <p class="mt-1 text-sm text-red-800">#{error_message.upcase_first}</p>
+           HTML
+           html
+         else
+           html_tag
+         end
+
+  html.html_safe
+end
