c044f8663c493957fd92169c5e4815539ad0b3d5
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-07-30 11:46:02 +0200

    apply ClassAndModuleChildren rule to fetcher services

--- app/services/search/fetcher.rb
+++ app/services/search/fetcher.rb
@@ -1,21 +1,19 @@
-module Search
-  class Fetcher
-    def initialize(model:, filter_params:, includes:, user:)
-      @model = model
-      @params = filter_params
-      @includes = includes
-      @user = user
-    end
+class Search::Fetcher
+  def initialize(model:, filter_params:, includes:, user:)
+    @model = model
+    @params = filter_params
+    @includes = includes
+    @user = user
+  end
 
-    def call
-      case @model.name
-      when "TrainingSession"
-        Search::TrainingSessionsFilter.new(filter_params: @params, includes: @includes, user: @user).call
-      when "User"
-        Search::UsersFilter.new(filter_params: @params, includes: @includes, user: @user).call
-      else
-        raise(ArgumentError, "Unsupported model type")
-      end
+  def call
+    case @model.name
+    when "TrainingSession"
+      Search::TrainingSessionsFilter.new(filter_params: @params, includes: @includes, user: @user).call
+    when "User"
+      Search::UsersFilter.new(filter_params: @params, includes: @includes, user: @user).call
+    else
+      raise(ArgumentError, "Unsupported model type")
     end
   end
 end
--- app/services/translations/fetcher.rb
+++ app/services/translations/fetcher.rb
@@ -1,29 +1,27 @@
-module Translations
-  class Fetcher
-    def initialize(translatable, language, field)
-      @translatable = translatable
-      @language = language
-      @field = field
-    end
+class Translations::Fetcher
+  def initialize(translatable, language, field)
+    @translatable = translatable
+    @language = language
+    @field = field
+  end
 
-    def call
-      Rails.cache.fetch(cache_key, expires_in: 12.hours) do
-        fetch_translation
-      end
+  def call
+    Rails.cache.fetch(cache_key, expires_in: 12.hours) do
+      fetch_translation
     end
+  end
 
-    private
+  private
 
-    def fetch_translation
-      @translatable.translations.find_by(language: @language, field: @field) || fallback_translation
-    end
+  def fetch_translation
+    @translatable.translations.find_by(language: @language, field: @field) || fallback_translation
+  end
 
-    def fallback_translation
-      @translatable.translations.find_by(language: "en", field: @field)
-    end
+  def fallback_translation
+    @translatable.translations.find_by(language: "en", field: @field)
+  end
 
-    def cache_key
-      "translation_#{@translatable.class.name}_#{@field}_#{@translatable.id}_#{@language}"
-    end
+  def cache_key
+    "translation_#{@translatable.class.name}_#{@field}_#{@translatable.id}_#{@language}"
   end
 end
